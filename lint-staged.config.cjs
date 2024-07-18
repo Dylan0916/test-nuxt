cc6b8a004d87d2fec8ca41a2522c13db88d9f9c4
Dylan <klj40702@gmail.com>
2024-07-18 22:56:59 +0800

    update

--- lint-staged.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  '*.{png,jpeg,jpg,gif,svg}': (files) => {
-    // 排除 before-assets 資料夾及其子資料夾中的所有文件
-    const filteredFiles = files.filter((file) => !/before-assets/.test(file))
-
-    return `imagemin-lint-staged ${filteredFiles.join(' ')}`
-  },
-}
