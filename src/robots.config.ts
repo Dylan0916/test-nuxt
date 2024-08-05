fa332c2ed32de6102a6e93963a50e1270affecf2
Dylan <klj40702@gmail.com>
2024-08-05 09:56:03 +0800

    delete robots.config.ts

--- src/robots.config.ts
+++
@@ -1,8 +0,0 @@
-const DISALLOW_PATH = ['/booking/', '/member/', '/orders/']
-const DISALLOW_FILE = ['/*.svg$']
-
-export default {
-  UserAgent: '*',
-  Disallow: () => [...DISALLOW_PATH, ...DISALLOW_FILE],
-  Sitemap: 'https://example.com/sitemap.xml',
-}
